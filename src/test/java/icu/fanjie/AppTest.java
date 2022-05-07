59cefd5cbacb66f1edc3ddd67ad35e7f4fa0a179
winner-hue <1344246287@qq.com>
2022-05-07 20:10:23 +0800

    update

--- src/test/java/icu/fanjie/AppTest.java
+++ src/test/java/icu/fanjie/AppTest.java
@@ -18,14 +18,4 @@ public class AppTest {
         System.out.println(jua.getChromeUA());
         System.out.println(jua.getFirefoxUA());
     }
-
-    @Test
-    public void test() throws Exception {
-        Headers headers = new Headers("user-agent:Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36");
-        Response response = Requests.request("get", "https://www.baidu.com", null, null, headers, 10000, true, null, null);
-        System.out.println(response.getHtml());
-        System.out.println(response.getStatusCode());
-        System.out.println(response.getCookie());
-        System.out.println(response.response);
-    }
 }
